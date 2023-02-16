34b8bc30f28afe7d6342eaeb309290554e5c86a8
jorgyamil <105092307+jorgyamil@users.noreply.github.com>
2023-02-15 20:34:15 -0500

    Add files via upload

---
+++ pages/watchMovie.js
@@ -0,0 +1,10 @@
+function AddON() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center md:flex-row">
+      Si llegaste hasta acá es porque quieres ver la serie.
+    Yo no puedo saber si llegaste hasta esta pagina jajaja asi que es tu turno en avisarme. 
+    </div>
+  );
+}
+
+export default AddON;
